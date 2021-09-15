308d400c3106b7580339974e38d0ed05f8e2d5a7
jefferson-s <jefferson.santos10@outlook.com>
2021-09-15 20:55:18 -0300

    new windows

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace DigitalInnovationOne
             for (int i=0; i < numerodevezes; i++)
             {
                 Console.WriteLine($"Bem vindo ao curso de .Net {i}");
+                Console.WriteLine($"new windows {i}");
             }
         }
     }
